2cdbc100ff5afb02651c77b7a072ae5a00d1cb35
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2024-03-30 18:53:52 +0530

    added whatsapp

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -3,13 +3,14 @@ import { Button } from "@mui/material";
 import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
 import MailIcon from '@mui/icons-material/Mail';
 import TextField from '@mui/material/TextField';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const ContactUs = () => {
   return (
     <div className="flex pt-[100px] w-full bg-[#FFFFFF] iPhoneSE:flex-col laptop:flex-row pb-[50px] gap-0 iPhoneSE:gap-[20px] laptop:gap-0">
       <div className="w-1/2 iPhoneSE:w-full laptop:w-1/2 flex pl-[5%] flex-col gap-[40px]">
         <h1 className="text-start font-bold text-4xl pr-[20%]">Need help? Get in touch now!</h1>
-        <div className="flex gap-[20px] items-center ">
+        <div onClick={() => window.open("tel:+91 7670848696", "_blank")} className="flex gap-[20px] items-center cursor-pointer">
           <div className="bg-[#FFF5EF] h-[60px] w-[60px] flex justify-center items-center rounded-xl">
             <PhoneInTalkIcon sx={{ color: "#FA6C2C" }} />
           </div>
@@ -18,7 +19,16 @@ const ContactUs = () => {
             <p className="text-start">+91 7670848696</p>
           </div>
         </div>
-        <div className="flex gap-[20px] items-center">
+        <div onClick={() => window.open("https://wa.me/7670848696", "_blank")} className="flex gap-[20px] items-center cursor-pointer">
+          <div className="bg-[#FFF5EF] h-[60px] w-[60px] flex justify-center items-center rounded-xl">
+            <WhatsAppIcon sx={{ color: "#FA6C2C" }} />
+          </div>
+          <div className="flex flex-start flex-col">
+            <p className="text-start">Whatsapp</p>
+            <p className="text-start">+91 7670848696</p>
+          </div>
+        </div>
+        <div onClick={() => window.open("mailto:udaykumarvalapudasu@gmail", "_blank")} className="flex gap-[20px] items-center cursor-pointer">
           <div className="bg-[#FFF5EF] h-[60px] w-[60px] flex justify-center items-center rounded-xl">
             <MailIcon sx={{ color: "#FA6C2C" }} />
           </div>
